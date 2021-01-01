43df981828346611ebcd8a5810399e99caca02c6
dione616 <Vetal160199@i.ua>
2021-01-01 14:36:14 +0200

    useReducer hook instead useState

--- client/src/lib/api/useMutation.ts
+++ client/src/lib/api/useMutation.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import { server } from "./server";
 
 interface State<TData> {
@@ -12,18 +12,46 @@ type MutationTuple<TData, TVariables> = [
   State<TData>
 ];
 
+type Action<TData> =
+  | { type: "DELETE" }
+  | { type: "DELETE_SUCCESS"; payload: TData }
+  | { type: "DELETE_ERROR" };
+
+const reducer = <TData>() => (state: State<TData>, action: Action<TData>) => {
+  switch (action.type) {
+    case "DELETE": {
+      return { ...state, loading: true };
+    }
+    case "DELETE_SUCCESS": {
+      return { ...state, loading: false };
+    }
+    case "DELETE_ERROR": {
+      return { ...state, loading: false, error: true };
+    }
+    default:
+      throw new Error("Error in reducer");
+  }
+};
+
 export const useMutation = <TData = any, TVariables = any>(
   query: string
 ): MutationTuple<TData, TVariables> => {
-  const [state, setState] = useState<State<TData>>({
+  const fetchReducer = reducer<TData>();
+  const [state, dispatch] = useReducer(fetchReducer, {
     data: null,
     loading: false,
     error: false,
   });
+  /* const [state, setState] = useState<State<TData>>({
+    data: null,
+    loading: false,
+    error: false,
+  }); */
 
   const fetch = async (variables?: TVariables) => {
     try {
-      setState({ data: null, loading: true, error: false });
+      dispatch({ type: "DELETE" });
+      /* setState({ data: null, loading: true, error: false }); */
 
       const { data, errors } = await server.fetch<TData, TVariables>({
         query,
@@ -34,9 +62,11 @@ export const useMutation = <TData = any, TVariables = any>(
         throw new Error(errors[0].message);
       }
 
-      setState({ data, loading: false, error: false });
+      dispatch({ type: "DELETE_SUCCESS", payload: data });
+      /* setState({ data, loading: false, error: false }); */
     } catch (error) {
-      setState({ data: null, loading: false, error: true });
+      dispatch({ type: "DELETE_ERROR" });
+      /* setState({ data: null, loading: false, error: true }); */
       throw console.error(error);
     }
   };
